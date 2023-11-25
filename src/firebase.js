a02f056b98b6ea90e1bc68f44e979c0d82d48b76
khosro <khosroshariatzadeh@gmail.com>
2023-11-24 22:23:34 -0500

    day 2

--- src/firebase.js
+++ src/firebase.js
@@ -17,6 +17,6 @@ const firebaseConfig = {
 
 
 // Initialize Firebase
-export const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig)
 export const auth = getAuth(app);
 export const db = getFirestore(app);
